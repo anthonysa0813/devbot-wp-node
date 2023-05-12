dde57646485463a963b7032ab509622fd7df9cde
anthony <anthony0813@gmail.com>
2023-05-12 10:44:43 -0500

    added logic to verifyToken controller

--- src/controllers/WhatsAppController.js
+++ src/controllers/WhatsAppController.js
@@ -1,7 +1,15 @@
 const verifyToken = (req, res) => {
-  return res.json({
-    message: "verify token",
-  });
+  try {
+    let accessToken = "TADG8A5TDRF2426YDAYDAD";
+    let token = req.query["hub.verify_token"];
+    let challenge = req.query["hub.challenge"];
+
+    if (challenge != null && token != null && token == accessToken) {
+      res.send(challenge);
+    }
+  } catch (error) {
+    res.status(400).send();
+  }
 };
 
 const receivedMessage = (req, res) => {};
