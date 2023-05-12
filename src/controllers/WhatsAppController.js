aca3dfddc59c10a419cfef1de07de411e2fde85c
anthony <anthony0813@gmail.com>
2023-05-12 11:15:02 -0500

    added logical in the controller to received messages

--- src/controllers/WhatsAppController.js
+++ src/controllers/WhatsAppController.js
@@ -1,3 +1,6 @@
+const fs = require("fs");
+const myConsole = new console.Console(fs.createWriteStream("./logs.txt"));
+
 const verifyToken = (req, res) => {
   try {
     let accessToken = "TADG8A5TDRF2426YDAYDAD";
@@ -12,7 +15,23 @@ const verifyToken = (req, res) => {
   }
 };
 
-const receivedMessage = (req, res) => {};
+const receivedMessage = (req, res) => {
+  try {
+    let entry = req.body["entry"][0];
+    let changes = entry["changes"][0];
+    let value = changes["value"];
+    let messageObject = value["messages"];
+    console.log("entre");
+    myConsole.log(messageObject);
+    console.log("entre");
+
+    return res.send("EVENT_RECEIVED");
+  } catch (error) {
+    // myConsole.log(error);
+    console.log(error);
+    return res.send("EVENT_RECEIVED");
+  }
+};
 
 module.exports = {
   verifyToken,
