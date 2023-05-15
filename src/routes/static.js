987ba9b417fa00a06f5ac708669613257eaf8637
anthony <anthony0813@gmail.com>
2023-05-15 11:14:48 -0500

    correct the path logs file}

--- src/routes/static.js
+++ src/routes/static.js
@@ -2,8 +2,9 @@ const { Router } = require("express");
 const router = Router();
 
 router.get("/", (req, res) => {
-  // res.sendFile(__dirname + "../../logs.txt");
-  res.send(__dirname + "../../logs.txt");
+  // console.log("logs.txt");
+  // res.send("logs.txt");
+  res.sendFile(__dirname + "../../logs.txt");
 });
 
 module.exports = router;
